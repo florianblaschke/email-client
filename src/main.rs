e06a1177a4237c56c380ef970dd5b4839314e6f8
Florian Blaschke <florianblaschke@web.de>
2025-09-08 21:14:28 +0200

    try build and connect with neon db

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use email_client::configuration::get_configuration;
 use email_client::startup::run;
 use email_client::telemetry::{get_subscriber, init_subscriber};
-use secrecy::ExposeSecret;
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 
 #[tokio::main]
@@ -13,8 +12,8 @@ async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration.");
 
     let connection_pool =
-        PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
-            .expect("Failed to connect to Postgres.");
+        PgPoolOptions::new().connect_lazy_with(configuration.database.connect_options());
+
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port
